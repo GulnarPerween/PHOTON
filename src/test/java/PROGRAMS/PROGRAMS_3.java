5f9847980a4f7bd467154282112c65a5f4a4d054
gperween0512@gmail.com <gperween0512@gmail.com>
2025-01-23 22:21:25 +0530

    THIS IS COMMIT DONE BY TESER 1

---
+++ src/test/java/PROGRAMS/PROGRAMS_3.java
@@ -0,0 +1,44 @@
+package PROGRAMS;
+
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class PROGRAMS_3 {
+	
+	@Test
+	public void method1() throws InterruptedException
+	{
+		
+	// WebDriver driver=new ChromeDriver();  
+	//WebDriver driver = new FirefoxDriver();  
+	WebDriver driver = new EdgeDriver();
+	
+     // Launch Website  
+    driver.navigate().to("https://www.google.com/");  
+  Thread.sleep(3000);
+   //Maximize the browser  
+    driver.manage().window().maximize();  
+    
+  
+ 
+    
+
+	
+
+   
+
+}  
+
+
+}
